bc031257e1c335ca3f5f443067edc206f27ca227
Manuel Rueda <rueda.manu@gmail.com>
2021-05-27 11:30:35 +0200

    Source commit

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ genome_ref_cal.sh: Script for inferring the genome reference from a vcf file. Wo
 
 This script allows to calculate the genome reference from a vcf file. We extracted unique nucleotides per pos in hg17, grch37, grch38.
 
-We select the chromosome with more variants in the vcf file and we run the script for that specific chromosome.
+We created a dictionary consisting of chr/pos/ref in hg17, grch37, grch38 reference genomes.
 
-It searchs for matches in 3 dictionaries (one per reference genome) and takes the one having matches (only one).
+The script searches for matches in the 3 dicts and outputs (infer_ref) the reference genome having more matches.
 
 A maximum of 10K variants per chromosome are analyzed. Also a maximum of 100 matches per chromosome are analyzed.
 
@@ -15,10 +15,10 @@ The script includes the dictionaries for inferring the genome reference.
 
 # Run
 
-Before running it for the first time you need to set the variable 'path_dic=' inside 'genome_ref_cal.sh'.
-In the installation directory the dictionaries are inside a folder name './ref_dics'
+Before running it for the first time you need to set the variable 'path_dic' inside 'genome_ref_cal.sh'.
+In the installation directory the dictionaries are inside a folder named './ref_dics'
 
-The script runs on Bash iand uses standard bash commands. The script was tested on Debian-based distributions.
+The script is written in _Bash_ and uses standard bash commands. The script was tested on Debian-based i(e.g., Ubuntu, Mint) distributions.
 
 Please note that input vcf file has to be gzipped (or bgzipped).
 
@@ -32,9 +32,9 @@ bash /path/genome_ref_cal.sh input.vcf.gz
 Demo folder contains a subset of vcf from chromosome 22 from 1000 Genomes data for testing purposes.
 
 1. \*.final: contains total number of matches for each genome reference.
-2. demo_subset.chr: file generated with the number of chromosomes and variants present in the demo vcf.
+2. \*.demo_subset.chr: file generated with the number of variants per chromosome present in the demo vcf.
 4. infer_ref: contains the inferred genome reference.
-5. log
+5. log: A log file of the STDOUT.
 
 # Author
 
@@ -43,4 +43,4 @@ Written by Dietmar Fernandez, PhD. Info about EGA can be found at https://ega-ar
 
 # Copyright
 
-This bash script is copyrighted. See the LICENSE file included in this distribution.
+This Bash script is copyrighted. See the LICENSE file included in this distribution.
--- genome_ref_cal.sh
+++ genome_ref_cal.sh
@@ -2,13 +2,14 @@
 
 ###################################################################
 # Script Name    : genome_ref_cal.sh
-# Date           : 2021-05-22
+# Date           : 2021-05-27
 # Version        : 3.5
 # Author         : Dietmar Fernandez (dietmar.fernandez@crg.eu)
 #                : Revised by Manuel Rueda (manuel.rueda@crg.eu)
 # usage          : bash genome_ref_cal.sh input.vcf.gz
-# Description    : This script allows to calculate the genome reference from a vcf file. We extracted unique nucleotides per pos in hg17, grch37, grch38 and created a dictionary.
-# Description    : It searchs for matches in 3 dictionaries (one per reference genome) and takes the one having matches (only one) generating a text file with the result.
+# Description    : This script infers the reference genome from a vcf file. 
+# Description    :  - We created a dictionary consisting of chr/pos/ref in hg17, grch37, grch38 reference genomes.
+# Description    :  - The script searches for matches in the 3 dicts and outputs (infer_ref) the reference genome having more matches.
 # Requirements   : gzip (tested version 1.10), awk (tested version Awk 5.0.1), grep (tested version 3.4), sed (tested sed (GNU sed) 4.7), wc (tested version (GNU coreutils) 8.30).
 ###################################################################
 
@@ -20,9 +21,8 @@ rand_var=10000 # 10K random variants
 match=100      # Nuber of matches for the grep   
 genomes=("hg17" "grch37" "grch38") # reference genomes
 
-# Path for dictionary files
-#path_dic="./ref_dics"
-path_dic="/media/mrueda/4TB/CRG_EGA/Project_QC/EGA_genomeref/EGA_genomeref-main/ref_dics"
+# Path for dictionary files (put here your own path)
+path_dic="./ref_dics"
 
 function usage {
 
@@ -46,31 +46,30 @@ base=$(basename $input_vcf .vcf.gz)
 # We get an id from bash process to be used when we append contents ( >> $$.file ) 
 echo "Job id $$"
 
-# STEP 1 - We get the list of chromosomes from the vcf along with the number of variants sorted reverse:
-echo "Getting chr|var list..."
-zcat $input_vcf | awk '{print $1}' | grep -v '^#' | sort | uniq -c | sort -r -n > $base.chr
-
-# STEP 2 - We read the input vcf once and split it by CHROM
-echo "Splitting by chr..."
+# STEP 1 - We split the file by CHROM into multiple files
+echo "Splitting vcf by chr..."
 zcat $input_vcf | cut -f1,2,4 | grep -v '^#' | awk '{print>$1".variants"}'
 
-# STEP 3 - For each chromosome we query a subset of variants against the dictionary
-for chr in $(awk '{print $2}' $base.chr | sort -V)
+# STEP 2 - For each chromosome we query a subset of variants against the dictionary
+for chr in $(ls -1 *.variants | awk -F'.' '{print $1}' |sort -V)
 do
   chr_str=$(echo $chr | sed 's/chr//')
   echo "Running chr$chr_str..."
+  
+  # First we add var|chr stats to $base.chr
+  { wc -l $chr.variants | awk '{print $1}' | tr -d '\n' ; echo " $chr_str"; } >> $$.$base.chr
 
-  # First we select 10K random variants
+  # Secondly we select 10K random variants
   shuf -n $rand_var $chr.variants | sort | sed 's/chr//g' > subset.$chr.variants
 
   # And finally we look for the first 100 matches in the 3 dictionaries
   for ref in ${genomes[@]}
   do
-   ( echo -n "$chr "; zgrep -m $match -Ef subset.$chr.variants $path_dic/subset.chr$chr_str.final.dic_$ref | wc -l ) >> $$.matches_$ref
+   { echo -n "$chr "; zgrep -m $match -Ef subset.$chr.variants $path_dic/subset.chr$chr_str.final.dic_$ref | wc -l; } >> $$.matches_$ref
   done 
 done
 
-# STEP 4 - We analyze the results
+# STEP 3 - We analyze the results
 for ref in ${genomes[@]}
 do
         tot_var=$(awk '{sum+=$2;}END{print sum;}' $$.matches_$ref)
@@ -79,7 +78,7 @@ do
 done
 sort -nk3 $$.final | tail -1 | awk '{print $NF}' > infer_ref
 
-# STEP 5 - Cleaning up
+# STEP 4 - Cleaning up
 rm $$.*matches* *.variants
 
 # End
